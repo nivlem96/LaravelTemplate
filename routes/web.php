b4381ab68f3cec19057fc74585008c3c15f7d188
nivlem <d.m.beverwijk@gmail.com>
2022-02-06 12:28:03 +0100

    Initial commit

---
+++ routes/web.php
@@ -0,0 +1,24 @@
+<?php
+
+use App\Http\Controllers\PublicController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', [PublicController::class, 'home']);
+
+Route::post('/locale', function () {
+
+    session(['app.locale' => app('request')->input('locale', 'en')]);
+
+    return redirect()->back();
+});
